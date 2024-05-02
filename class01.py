855ad39caf3866db2d923010eb6df765bb8a7bd9
Crimson-Raven <becks2028@gmail.com>
2024-05-01 19:50:50 -0500

    05/01/2024

---
+++ class01.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+# Script: Ops 401 Challenge 01
+# Author: Rebecca Childs
+# Date of last revision: 
---
+++ class03.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+# Script: Ops 401 Challenge 03
+# Author: Rebecca Childs
+# Date of last revision: 05/01/2024
